aadd255a08291cb3d1594484dd0d93d3dd3e3525
Mykellaguirre <67997603+Mykellaguirre@users.noreply.github.com>
2022-02-14 13:36:11 -0800

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,8 @@
 <?
-   $name = "Mykel Aguirre";
-   echo "Hello ".$name;
+   
+// Read the JSON file
+$json_file = file_get_contents('my_data.json');
+
+// Decode the JSON file
+$json_data = json_decode($json_file,true);
 ?>
